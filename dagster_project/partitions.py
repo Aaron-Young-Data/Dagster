72a57619fc26dc61a9823c034973381cb8d1d47d
Aaron Young <52836687+Aaron-Young-Data@users.noreply.github.com>
2024-04-14 14:17:13 +0100

    timezone change

--- dagster_project/partitions.py
+++ dagster_project/partitions.py
@@ -4,4 +4,4 @@ from dagster import DailyPartitionsDefinition
 today = datetime.today()
 partition_start_date = today - timedelta(days=14)
 
-daily_partitions = DailyPartitionsDefinition(start_date=partition_start_date, end_offset=1)
+daily_partitions = DailyPartitionsDefinition(start_date=partition_start_date, end_offset=1, timezone="GMT")
