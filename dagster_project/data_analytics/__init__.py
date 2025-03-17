1c9ae75ae462bf0383cd82526ef034952aa9b72d
Aaron Young <52836687+Aaron-Young-Data@users.noreply.github.com>
2025-03-17 20:02:13 +0000

    Adding local_artifact_storage due to storage issues on the box that is running the instance

--- dagster_project/data_analytics/__init__.py
+++ dagster_project/data_analytics/__init__.py
@@ -13,13 +13,17 @@ all_assets = [*data_load_assets,
 
 defs = Definitions(
     assets=all_assets,
-    jobs=[download_all_session_data_job,
-          data_load_track_status_data_job,
-          data_load_weekend_session_data
-          ],
+    jobs=[
+        download_all_session_data_job,
+        data_load_track_status_data_job,
+        data_load_weekend_session_data,
+        data_load_all_session_data_job
+    ],
     schedules=[update_track_status_data_job_weekly_schedule],
-    sensors=[weekend_session_data_load_job_sensor,
-             session_data_download_job_sensor],
+    sensors=[
+        weekend_session_data_load_job_sensor,
+        session_data_download_job_sensor
+    ],
     resources={
         'sql_io_manager': sql_io_manager.SQLIOManager(
             user=os.getenv('SQL_USER'),
--- dagster_project/data_analytics/jobs.py
+++ dagster_project/data_analytics/jobs.py
@@ -6,13 +6,22 @@ from .assets import *
 from .assets.data_download.all_session_data import *
 from .assets.data_load.all_sessions import *
 from .assets.data_load.track_status import *
-
 from .partitions import daily_partitions
 
 download_all_session_data_job = define_asset_job('download_all_session_data_job',
                                                  selection=AssetSelection.assets(all_session_data_from_sql,
                                                                                  all_session_data_to_csv),
                                                  description='Job to download all of the session data')
+
+data_load_all_session_data_job = define_asset_job('data_load_all_session_data_job',
+                                                  selection=AssetSelection.assets(get_data_analysis_session_data,
+                                                                                  data_analysis_session_data_to_sql),
+                                                  description='Job to load all of the session data',
+                                                  config={'ops':
+                                                              {'get_data_analysis_session_data':
+                                                                   {"config":
+                                                                        {'year_list': [2018, 2019, 2020, 2021, 2022, 2023, 2024]}
+                                                                    }}})
 
 data_load_track_status_data_job = define_asset_job('data_load_track_status_data_job',
                                                    selection=AssetSelection.assets(get_track_status_data_csv,
