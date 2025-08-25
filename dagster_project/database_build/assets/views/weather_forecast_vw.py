cdf399d5829e883561df58dbe4aa7bed7cd5d101
Aaron Young <52836687+Aaron-Young-Data@users.noreply.github.com>
2025-08-25 14:56:42 +0100

    Adding weather vw

--- dagster_project/database_build/assets/views/weather_forecast_vw.py
+++ dagster_project/database_build/assets/views/weather_forecast_vw.py
@@ -11,9 +11,21 @@ port = os.getenv('SQL_PORT')
 server = os.getenv('SQL_SERVER')
 
 
-@asset(deps=['create_f1_calender', 'create_dim_track', 'create_dim_track_event'])
-def create_weather_forecast_view(context, create_weather_forcast):
+@asset(deps=['create_weather_forcast'])
+def create_weather_forecast_view(context):
     query = FileUtils.file_to_query('create_weather_forecast_vw')
+    context.log.info(f'Query to run: \n{query}')
+    con = MySQLDirectConnection(port, database, user, password, server)
+    df = con.run_query_no_output(query=query)
+
+    return Output(
+        value=df
+    )
+
+
+@asset(deps=['create_weather_forecast_view'])
+def create_weather_forecast_view(context):
+    query = FileUtils.file_to_query('create_weather_vw')
     context.log.info(f'Query to run: \n{query}')
     con = MySQLDirectConnection(port, database, user, password, server)
     df = con.run_query_no_output(query=query)
